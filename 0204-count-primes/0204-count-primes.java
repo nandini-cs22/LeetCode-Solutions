74e4bdcf28b5ada9fd205c766be2ea970c793264
nandini-cs22 <122636297+nandini-cs22@users.noreply.github.com>
2025-09-12 09:50:20 +0530

    Time: 94 ms (75.76%), Space: 49.2 MB (57.09%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,23 +1,26 @@
 class Solution {
     public int countPrimes(int n) {
-        if (n <= 2) return 0;
-
+        if(n <= 2){
+            return 0;
+        }
         boolean[] isPrime = new boolean[n];
-        Arrays.fill(isPrime, true);
+        Arrays.fill(isPrime,true);
+
         isPrime[0] = false;
         isPrime[1] = false;
 
-        for (int i = 2; i * i < n; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    isPrime[j] = false;
+        for(int p = 2;p * p < n;p++){
+            if(isPrime[p]){
+                for(int i = p*p;i<n;i+=p){
+                    isPrime[i] =  false;
                 }
             }
         }
-
         int count = 0;
-        for (int i = 2; i < n; i++) {
-            if (isPrime[i]) count++;
+        for(int i = 2;i<n;i++){
+            if(isPrime[i]){
+                count++;
+            }
         }
         return count;
     }
